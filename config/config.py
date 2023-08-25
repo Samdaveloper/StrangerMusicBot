39d099bd74a828f5a94c98c3ee60ab97d9e28804
Sam_x_developer <129406748+Samdaveloper@users.noreply.github.com>
2023-08-25 18:34:52 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -49,7 +49,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/sparrow9616/StrangerMusicBot",
+    "https://github.com/samdaveloper/StrangerMusicBot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
